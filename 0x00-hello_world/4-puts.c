d9e31aad8ec337f9559308a3d86548b0f5037f30
fiifi-oa <koppongamoah@gmail.com>
2022-10-13 13:39:09 -0700

    nuts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 
 #include <stdio.h>
 
+/**
+ * main - printing
+ *
+ * Return: always 0 (true)
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
-/** always prints this statement */
