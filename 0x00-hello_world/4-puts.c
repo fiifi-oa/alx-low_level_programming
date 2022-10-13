6d9134eb9dc173f203508693bc8fbe0dc5ff8100
fiifi-oa <koppongamoah@gmail.com>
2022-10-13 13:31:01 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 
 #include <stdio.h>
 
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
-
 }
-/** always prints this statement */ 
+/** always prints this statement */
