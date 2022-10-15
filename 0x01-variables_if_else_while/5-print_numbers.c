cbc68002e07f75812359f9b57237bff7214d3879
fiifi-oa <koppongamoah@gmail.com>
2022-10-15 00:13:19 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.c>
+
+/**
+ * main - print numbers
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	for (; a < 10; a++)
+		printf("%d \n", a);
+
+return (0);
+}
