f695ad24bef2b7e475fedfea9ed6fcd12a449744
fiifi-oa <koppongamoah@gmail.com>
2022-10-14 23:40:18 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case aphabets without q and e
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		do putchar(alp);
+		while (alp != e || q);
+	}
+
+	return (0);
+}
