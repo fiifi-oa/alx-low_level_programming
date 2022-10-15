1243e08566ada70997d92b0114501e351296116f
fiifi-oa <koppongamoah@gmail.com>
2022-10-15 06:36:16 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int comb;
 
-	for (comb = 38; comb < 68 ; comb++)
+	for (comb = 18; comb < 98 ; comb++)
 	{
 		putchar(comb);
-		if (comb != 67)
+		if (comb != 97)
 		{
 			putchar(',');
 			putchar(' ');
