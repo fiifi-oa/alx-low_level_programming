89a8f0efa5bb54c067deaa6986449185af75de79
fiifi-oa <koppongamoah@gmail.com>
2022-10-15 06:06:03 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -  print numbers in base 16
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int bsix;
+	char basix;
+
+	for (bsix = 0;  bsix < 10; bsix++)
+		putchar (bsix % 10);
+
+	for (basix = 'a'; basix < 'f'; basix++)
+		putchar(basix);
+
+	putchar('\n');
+
+	return (0);
+}
