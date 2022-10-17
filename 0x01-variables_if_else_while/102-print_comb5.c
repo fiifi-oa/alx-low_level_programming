aec8b9b8d7fadcaa7d6af3f69ae0d4a28dd79416
fiifi-oa <koppongamoah@gmail.com>
2022-10-17 09:16:38 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,24 @@
 
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 99; a++)
 	{
-		for (b = 0; b < 9; b++)
+		for (b = 1; b < 100; b++)
 		{
-			for (c = 0; c < 10; c++)
-			{
-				for (d = c + 1; d < 10; d++)
-				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(' ');
-					putchar(c + '0');
-					putchar(d + '0');
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 
 
-					if (a == 9 && b == 8 && c == 9 && d == 9)
+					if (a == 98 && b == 99)
 						break;
 
 					putchar(',');
 					putchar(' ');
-				}
-			}
 		}
 	}
 	putchar('\n');
