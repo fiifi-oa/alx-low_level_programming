d352c38245fbf55ca9fbb57559aa21c938152e7b
fiifi-oa <koppongamoah@gmail.com>
2022-10-17 08:12:14 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of three digits
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 1; b < 9; b++)
+		{
+			for (c = 2; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+
+				if (a == 7 && b == 8 && c == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
