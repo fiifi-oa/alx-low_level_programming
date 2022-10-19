603048ba1bd8f304805b735280c7f83e02283e5c
fiifi-oa <koppongamoah@gmail.com>
2022-10-18 20:48:27 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * _isalpha - checks for alphabet
+ * @c: - character to be checcked
+ *
+ * Return: 0 if false, 1 if true
+ */
+
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
