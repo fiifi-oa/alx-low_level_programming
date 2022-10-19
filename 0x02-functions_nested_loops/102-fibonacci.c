d7cbf1081ee91c5e51d77d8efe1814b35ffe617e
fiifi-oa <koppongamoah@gmail.com>
2022-10-19 13:06:37 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <main.h>
+
+/**
+ * main - print fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+
+	int onum = 1;
+
+	while (onum < 50)
+	{
+		int c = a + b;
+
+		printf("%d, ", c);
+
+		a = b;
+		b = c;
+
+		onum++;
+	}
+	return (0);
+}
