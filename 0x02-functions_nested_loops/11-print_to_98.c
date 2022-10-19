8ecaac36b36946a860e9f9026b9ad3a5fa346d41
fiifi-oa <koppongamoah@gmail.com>
2022-10-18 22:38:39 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n: where to start
+ *
+ * Return: n
+ */
+
+void print_to_98(int n)
+{
+	for (; n >= 98; n++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+
+
+		if (n == 98)
+			break;
+	}
+	_putchar('\n');
+	return (n);
+}
