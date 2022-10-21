71b7f2162d7c70a5ddca77fc30cb610cb29f801c
fiifi-oa <koppongamoah@gmail.com>
2022-10-20 22:18:34 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,11 @@ void more_numbers(void);
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 15; b++)
+		{
+			if (b > 10)
+				_putchar((b / 10) + '0');
 			_putchar((b % 10) + '0');
-
+		}
 		_putchar('\n');
 	}
 }
