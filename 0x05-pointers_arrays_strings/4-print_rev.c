d5020736203bab2712a90f0941255b8c8843a0f5
fiifi-oa <koppongamoah@gmail.com>
2022-10-24 13:28:08 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev -  print string in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*str != '\0')
+		i++;
+
+	for (1 = 1 - 1; i >= 0; i--)
+		_putchar(i);
+
+	_putchar('\n');
+}
