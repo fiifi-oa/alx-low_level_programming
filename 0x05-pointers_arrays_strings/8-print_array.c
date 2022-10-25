62c824e677d3f4c6f35fb0bb19db68ab3a7af4bb
fiifi-oa <koppongamoah@gmail.com>
2022-10-24 21:52:28 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - print n elements
+ * @n: number of elements
+ * @a: array
+ */
+
+void print_array(int *a, int n)
+{
+	int num;
+
+	for (num = 0; num < n; num++)
+	{
+		printf("%d", a[num]);
+		if (num != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
