df151986ecde7a7d1d0a6ebb3ebedd33543e90fd
fiifi-oa <koppongamoah@gmail.com>
2022-11-14 18:58:40 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * sruct dog - a new type describing a dog
+ * struct dog - a new type describing a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -16,10 +16,11 @@ struct dog
 }
 
 /**
- * dog_t -  typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
