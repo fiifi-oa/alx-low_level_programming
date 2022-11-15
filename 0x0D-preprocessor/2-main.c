fa4db8e1a437f8615896029dfd0771fcf3d2cf06
fiifi-oa <koppongamoah@gmail.com>
2022-11-14 18:17:25 -0800

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,12 @@
 
 /**
  * main -  prints the name of the file it was compiled from
- * return: 0
+ * Return: 0
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return(0);
+	return (0);
 }
